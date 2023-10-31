edff47653dd1ddc67d33ac96402911a1636a77d9
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-31 21:57:29 +0800

    first data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
 
 mongoose.connect(
   "mongodb+srv://user123:luffyboy2022@cluster0.ywm0jzz.mongodb.net/merntutorial?retryWrites=true&w=majority"
 );
+
+app.get("/getUsers", (req, res) => {
+  UserModel.find({});
+});
+
+app.post();
 
 app.listen(3001, () => {
   console.log("server runs perfectly");
