09cf448e0824aec9e03ecc5445822261df1febde
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-10-31 16:04:25 +0800

    mongoodb compass > '

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb+srv://user123:luffyboy2022@cluster0.ywm0jzz.mongodb.net/"
+);
+
 app.listen(3001, () => {
   console.log("server runs perfectly");
 });
